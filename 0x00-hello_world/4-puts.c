2ceefcdde9de80adb5f7875396061133b077c57d
Lekara Nwisane <michaellek2014@gmail.com>
2022-09-08 20:48:47 -0700

    Program to print with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-/* Program to print with puts */
+/**
+ * main - Entry point
+ *
+ * puts - print function
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("''Programming is like building a multilingual puzzle\n");
 	return (0);
 }
