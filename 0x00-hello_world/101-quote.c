6403815354af0f29795ca8efd25b964cd3b85728
Lekara Nwisane <michaellek2014@gmail.com>
2022-09-09 14:46:34 -0700

    Script to generate assembly code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * fwrite - Display output
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	fwrite(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (0);
+}
