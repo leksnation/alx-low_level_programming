b7b225a81fc58177d0b5c33ea1cb2c9c375ff815
Lekara Nwisane <michaellek2014@gmail.com>
2022-09-10 17:41:53 -0700

    Program to check last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -27,7 +27,7 @@ int main(void)
 	}
 	else if (rem < 0)
 	{
-		printf("Last digit of %d is %d and is less that 6 and not 0\n", n, rem);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
 	}
 
 	return (0);
