d889d0c03f58ec32e01222f559265b01b909cb3d
Lekara Nwisane <michaellek2014@gmail.com>
2022-09-12 17:31:11 -0700

    Prints all possible different combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,7 +19,7 @@ int main(void)
 			{
 				putchar((a % 10) + '0');
 				putchar((b % 10) + '0');
-				if (a != 18 || b != 19)
+				if (b != 18 || a != 19)
 				{
 					putchar(',');
 					putchar(' ');
