67e62da91332bbc9cdac14a8e0e5007d6c9f87cd
Lekara Nwisane <michaellek2014@gmail.com>
2022-09-17 06:06:56 -0700

    Updated fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,21 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else if ((i % 3) == 0)
+		else if (i % 3 == 0)
 		{
 			printf("FiZZ ");
 		}
-		else if ((i % 5) == 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d", i);
+			printf("%i", i);
 		}
 		if (i != 100)
 		{
